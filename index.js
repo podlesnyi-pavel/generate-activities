3cad458d6faae743830f72e35249d27d04222111
Pavel Podlesnyi <podlesnyi.pavel.v@gmail.com>
2023-10-25 13:05:54 +0300

    init project on github

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict'
+
+async function getActivity() {
+  const response = await fetch('http://www.boredapi.com/api/activity/');
+  const data = await response.json();
+
+  return data;
+}
+
+async function createActivityCards() {
+  try {
+    const activities = await Promise.all([
+      getActivity(),
+      getActivity(),
+      getActivity(),
+    ]);
+  
+    const cardsWrapper = document.querySelector('.cards-wrapper');
+    cardsWrapper.innerHTML = '';
+  
+    for (const activity of activities) {
+      const cardDiv = document.createElement('div');
+      cardDiv.innerText = activity.activity;
+      cardDiv.classList.add('card');
+      cardsWrapper.append(cardDiv);
+    }
+  } catch (error) {
+    throw new Error('Error: ' + error);
+  }
+}
+
+document.querySelector('button').addEventListener('click', createActivityCards);
+
+createActivityCards();
